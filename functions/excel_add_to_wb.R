e8bd05b2a4f850602be20bb67fa7300dbeeb3675
greglenane <gregory.lenane@mssm.edu>
2025-10-14 15:27:10 -0400

    complete revamp of excel wb function for new output format

--- functions/excel_add_to_wb.R
+++ functions/excel_add_to_wb.R
@@ -1,82 +1,79 @@
-# excel wb function
-add_to_wb <- function(df, sheetname, add_filter = FALSE) {
-  # create sheet name
-  sheet <- addWorksheet(wb, sheetname)
+add_to_wb <- function(df, sheetname) {
   
-  setColWidths(wb, sheetname, cols = 1:ncol(df), widths = "auto")
+  # create sheet
+  addWorksheet(wb, sheetname)
   
-  # write data to sheet
+  # write data (no column names)
   writeData(wb,
-            x = df,
             sheet = sheetname,
+            x = df,
             startRow = 4,
             startCol = 1,
             colNames = FALSE,
-            keepNA = FALSE,
-            withFilter = add_filter)
+            keepNA = FALSE)
   
-  # add headers
-  # Hospital row ---------------------------------------------------------------
-  for (i in 1:length(hospitals)) {
-    writeData(wb,
-              sheetname,
-              x = hospitals[[i]],
-              startRow = 1,
-              startCol = 2 + ((i-1)*10))
-    
-    mergeCells(wb, sheetname, rows = 1,
-               cols = (2 + ((i-1)*10)):(1 + ((i)*10)))
+  # Title row ------------------------------------------------------------------
+  # Baseline title
+  writeData(wb,
+            sheetname,
+            x = paste0("Baseline (", 
+                       format(min(baseline$SERVICE_DATE), "%B%Y"), " - ",
+                       format(max(baseline$SERVICE_DATE), "%B%Y"),
+                       ")"),
+            startRow = 1,
+            startCol = 3)
+  mergeCells(wb, sheetname, rows = 1, cols = 3:6)
+  
+  # Scenario title
+  writeData(wb,
+            sheetname,
+            x = "Projections",
+            startRow = 1,
+            startCol = 7)
+  mergeCells(wb, sheetname, rows = 1, cols = 7:10)
+
+  # Metric row -----------------------------------------------------------------
+  # Hospital and Unit Type headers
+  writeData(wb, sheetname, 
+            x = t(as.data.frame(c("Hospital", "Unit Type"))), 
+            startRow = 2, 
+            startCol = 1,
+            colNames = FALSE)
+  # Baseline and scenario metrics
+  metrics <- c("Avg. Bed Capacity", "Avg.Daily Demand", "Avg. Bed Utilization", "Days over 85%")
+  writeData(wb,
+            sheetname,
+            x = t(as.data.frame(rep(metrics, 2))),
+            startRow = 2,
+            startCol = 3,
+            colNames = FALSE)
+  for (i in 1:ncol(df)) {
+    mergeCells(wb, sheetname, rows = 2:3, cols = i)
   }
+  mergeCells(wb, sheetname, rows = 1, cols = 1:2)
   
-  # metrics row ----------------------------------------------------------------
-  for (i in 1:length(hospitals)) {
-    writeData(wb, sheetname, x = "Bed Capacity", startRow = 2, startCol = 2 + ((i-1)*10))
-    mergeCells(wb, sheetname, rows = 2, cols = (2 + ((i-1)*10)):(2 + ((i-1)*10) + 1))
-    
-    writeData(wb, sheetname, x = "Daily Bed Demand", startRow = 2, startCol = 4 + ((i-1)*10))
-    mergeCells(wb, sheetname, rows = 2, cols = (4 + ((i-1)*10)):(4 + ((i-1)*10) + 1))
-    
-    writeData(wb, sheetname, x = "Avg. Bed Utilization", startRow = 2, startCol = 6 + ((i-1)*10))
-    mergeCells(wb, sheetname, rows = 2, cols = (6 + ((i-1)*10)):(6 + ((i-1)*10) + 1))
-    
-    writeData(wb, sheetname, x = "Days over 85%", startRow = 2, startCol = 8 + ((i-1)*10))
-    mergeCells(wb, sheetname, rows = 2, cols = (8 + ((i-1)*10)):(8 + ((i-1)*10) + 1))
-    
-    writeData(wb, sheetname, x = "Days over 95%", startRow = 2, startCol = 10 + ((i-1)*10))
-    mergeCells(wb, sheetname, rows = 2, cols = (10 + ((i-1)*10)):(10 + ((i-1)*10) + 1))
-  }
-  
-  # baseline scenario ----------------------------------------------------------
-  subheader <- rep(c("Baseline", "Scenario"), 5)
-  for (i in 1:length(hospitals)) {
-    writeData(wb, sheetname, x = matrix(subheader, nrow = 1, byrow = TRUE), startRow = 3, startCol = 2 + ((i-1)*10), colNames = FALSE)
-  }
-  
-  # unit type ------------------------------------------------------------------
-  writeData(wb, sheetname, x = "Unit Type", startRow = 3, startCol = 1)
-  
-  #create cell styles
-  header_style <- createStyle(fgFill = "grey", halign = "center", textDecoration = "bold", fontColour = "#010101")
+  # Styles ---------------------------------------------------------------------
+  # Create styles
+  hosp_style <- createStyle(fgFill = "#221F72", halign = "left", textDecoration = "bold", fontColour = "#FFFFFF")
+  baseline_style <- createStyle(fgFill = "#bcdfeb", halign = "center", textDecoration = "bold", fontColour = "#010101", wrapText = TRUE)
+  scenario_style <- createStyle(fgFill = "#ffcad2", halign = "center", textDecoration = "bold", fontColour = "#010101", wrapText = TRUE)
   body_style <- createStyle(border = "TopBottomLeftRight")
   numeric_style <- createStyle(numFmt = "0.00")
   percent_style <- createStyle(numFmt = "0%")
   
-  # apply numeric formatting
-  for (i in 1:length(hospitals)) {
-    addStyle(wb, sheetname, numeric_style, rows = 4:(nrow(df) + 3), cols = (2 + ((i-1)*10)):(5 + ((i-1)*10)), gridExpand = TRUE, stack = TRUE)
-  }
+  # Apply data styles
+  addStyle(wb, sheetname, numeric_style, rows = 4:(nrow(df) + 3), cols = c(3:4, 7:8), gridExpand = TRUE, stack = TRUE)
+  addStyle(wb, sheetname, percent_style, rows = 4:(nrow(df) + 3), cols = c(5:6, 9:10), gridExpand = TRUE, stack = TRUE)
   
-  # apply percentage formatting
-  for (i in 1:length(hospitals)) {
-    addStyle(wb, sheetname, percent_style, rows = 4:(nrow(df) + 3), cols = (6 + ((i-1)*10)):(11 + ((i-1)*10)), gridExpand = TRUE, stack = TRUE)
-  }
+  # Apply header styles
+  addStyle(wb, sheetname, hosp_style, rows = 2:3, cols = 1:2, gridExpand = TRUE, stack = TRUE)
+  addStyle(wb, sheetname, baseline_style, rows = 1:3, cols = 3:6, gridExpand = TRUE, stack = TRUE)
+  addStyle(wb, sheetname, scenario_style, rows = 1:3, cols = 7:10, gridExpand = TRUE, stack = TRUE)
   
-  # apply cell styles
-  addStyle(wb, sheetname, header_style, rows = 1:3, 
-           cols = 1:ncol(df), gridExpand = TRUE, stack = TRUE)
-  addStyle(wb, sheetname, body_style, rows = 1:(nrow(df) + 3),
-           cols = 1:ncol(df), gridExpand = TRUE, stack = TRUE)
+  # Apply body borders
+  addStyle(wb, sheetname, body_style, rows = 1:(nrow(df) + 3), cols = 1:ncol(df), gridExpand = TRUE, stack = TRUE)
   
-  freezePane(wb, sheetname, firstCol = TRUE)
+  # auto column widths
+  setColWidths(wb, sheetname, cols = 1:ncol(df), widths = "auto")
   
 }

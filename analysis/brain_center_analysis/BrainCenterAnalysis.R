6e7fae2dc6b69a88356e6d2833711c9bfd0a92d3
Dheeraj Tommandru <tommad01@mssm.edu>
2026-04-17 16:44:58 -0400

    Using Updated tables and checking for commonality

--- analysis/brain_center_analysis/BrainCenterAnalysis.R
+++ analysis/brain_center_analysis/BrainCenterAnalysis.R
@@ -22,7 +22,6 @@ library(dbplyr)
 library(glue)
 library(assertr)
 library(doParallel)
-# library(blastula)
 library(readr)
 library(zip)
 library(here)
@@ -30,14 +29,11 @@ library(hms)
 library(ggplot2)
 library(patchwork)
 library(grid)
-# library(ggnewscale)
 library(ggtext)
 library(tidyverse)
 library(shadowtext)
 
-# library(magick)
 
-# library(ggh4x) # for x axis
 
 # source functions ----
 # capacity modeling path
@@ -73,59 +69,60 @@ cap_dir <- "/SharedDrive/deans/Presidents/HSPI-PM/Operations Analytics and Optim
 # neuro_encounters <- unique(neuro_bed_charges$ENCOUNTER_NO)
 
 # --------------------- Table Names ---------------------
-or_encounter_data_table_name <- 'MS_INSIGHT.OR_QUALITY_DASHBOARD_CASE_DETAILS'
-or_room_master_data_table_name <- 'MS_INSIGHT.OR_QUALITY_DASHBOARD_ROOM_MASTER'
-or_room_schedules_data_table_name <- 'MS_INSIGHT.OR_QUALITY_ROOM_UTIL_V'
-ip_encounter_table_name <- 'MSX_IP_OUTPUT'
+ip_or_master_table_name <- 'IPCAP_OR_CASE_DATA'
 
 # --------------------- Filters ---------------------
 # current_date <- Sys.Date()
 # sched_date <- '2024-09-01'
-sched_start_date <- '2025-01-01'
+# sched_start_date <- '2025-01-01'
 # sched_end_date <- '2026-03-31'
 status <- 'Completed'
-facilities <- "('MSH','RVT','STL')"
+# facilities <- "('MSH','RVT','STL')"
 # room_exclusion_list <- "('MSW OR 23','MSM OR 08','MSM OR 15')"
-# read in neuro codes and isolate DRGs
-neuro_codes <- read_csv(paste0(cap_dir,"Adhoc/MS Brain Health/","NeuroSurgeryCodes.csv")) 
-neuro_drg <- neuro_codes$`Ms Drg`
+
+
+# read in neuro codes and isolate DRGs/CPTs
+neuro_codes_drg <- read_csv(paste0(cap_dir,"Adhoc/MS Brain Health/Mappings/","Neurosurgery DRG.csv")) 
+neuro_drg <- neuro_codes_drg$`MS-DRG`
 neuro_drg_query <- paste0("('", paste(neuro_drg, collapse = "', '"), "')")
+
+neuro_codes_cpt <- read_csv(paste0(cap_dir,"Adhoc/MS Brain Health/Mappings/","Neurosurgery CPT.csv")) 
+neuro_cpt <- neuro_codes_cpt$`CPT Code`
+neuro_cpt_query <- paste0("('", paste(neuro_cpt, collapse = "', '"), "')")
 
 
 
 # query to capture combining IP and OR data ----
-ip_or_query <- glue("select d.pat_mrn_id               as or_pat_mrn_id,
-                            d.patient_in_room_dttm     as or_patient_in_room_dttm,
-                            d.patient_out_room_dttm    as or_patient_out_room_dttm,
-                            d.or_case_id               as or_case_id,
-                            d.case_status              as or_case_status,
-                            d.turnover_from_prior_case as or_turnover_from_prior_case,
-                            d.PRIMARY_PROC_CODE        as or_primary_procedure_code
-                            rm.room_id                 as or_rm_room_id,
-                            rm.location_nm             as or_rm_location_name,
-                            rm.cluster_name            as or_rm_cluster_name,
-                            i.encounter_no             as msxipoutput_encounter_no,
-                            i.admit_dt_src             as msxipoutput_admit_dt_src,
-                            i.dsch_dt_src              as msxipoutput_dsch_dt_src,
-                            i.msdrg_cd_src             as msxipoutput_msdrg_cd_src,
-                            i.msdrg_desc_msx           as msxipoutput_msdrg_desc_msx,
-                            i.facility_msx             as msxipoutput_facility_msx
-                    FROM {or_encounter_data_table_name} d 
-                    LEFT JOIN {room_master_data_table_name} rm
-                        ON d.or_id = rm.room_id
-                    LEFT JOIN {ip_encounter_table_name} i 
-                        ON i.MRN_MSX = d.PAT_MRN_ID and d.PATIENT_IN_ROOM_DTTM between i.ADMIT_DT_SRC and i.DSCH_DT_SRC
-                    where d.PATIENT_IN_ROOM_DTTM is not null and
-                          d.SURGERY_DATE >= TO_DATE('{sched_start_date}','YYYY-MM-DD') AND
-                          i.MSDRG_CD_SRC IN {neuro_drg_query} AND
-                          i.FACILITY_MSX IN {facilities};")
+ip_or_query_drg_cpt <- glue("SELECT *
+                              FROM {ip_or_master_table_name} d 
+                              WHERE d.CASE_STATUS = '{status}'and
+                                    d.MSDRG_CD_SRC IN {neuro_drg_query} AND
+                                    d.PRIMARY_PROC_CODE IN {neuro_cpt_query};")
+ip_or_query_drg <- glue("SELECT *
+                              FROM {ip_or_master_table_name} d 
+                              WHERE d.CASE_STATUS = '{status}' AND
+                                    d.MSDRG_CD_SRC IN {neuro_drg_query};")
+ip_or_query_cpt <- glue("SELECT *
+                              FROM {ip_or_master_table_name} d 
+                              WHERE d.CASE_STATUS = '{status}' AND
+                                    d.PRIMARY_PROC_CODE IN {neuro_cpt_query};")
+
+
+ip_or_query_null_cpt_drg_neuro_speciality <- glue("SELECT *
+                                                  FROM {ip_or_master_table_name} d 
+                                                  WHERE REGEXP_LIKE(SURGEON_SPECIALTY, 'NEURO') AND
+                                                        MSDRG_CD_SRC IS NULL AND
+                                                        PRIMARY_PROC_CODE IS NULL;")
 
 
 # Establish DB Connection and Get data ----
 dsn <- "OAO Cloud DB Production"
 conn <- dbConnect(odbc(), dsn)
 dbExecute(conn, "ALTER SESSION SET TIME_ZONE = 'America/New_York'")
-ip_or_data <- dbGetQuery(conn,"SELECT * FROM IPCAP_OR_CASE_DATA")
+ip_or_data_drg_cpt <- dbGetQuery(conn,ip_or_query_drg_cpt)
+ip_or_data_drg <- dbGetQuery(conn,ip_or_query_drg)
+ip_or_data_cpt <- dbGetQuery(conn,ip_or_query_cpt)
+ip_or_data_null_cpt_drg_neuro_speciality <- dbGetQuery(conn,ip_or_query_null_cpt_drg_neuro_speciality)
 dbDisconnect(conn)
 
 
